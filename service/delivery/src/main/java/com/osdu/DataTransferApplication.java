48226ce3deec6515431a5f1bea895d84377408d1
Rodion Shkrobot <rshkr@softserve.com>
2019-09-06 15:18:49 +0300

    OSDU-33: Feign clients to work with delfi portals

--- service/delivery/src/main/java/com/osdu/DataTransferApplication.java
+++ service/delivery/src/main/java/com/osdu/DataTransferApplication.java
@@ -2,8 +2,10 @@ package com.osdu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"com.osdu"})
 public class DataTransferApplication {
 
     public static void main(String[] args) {
---
+++ service/delivery/src/main/java/com/osdu/client/delfi/DelfiDeliveryClient.java
@@ -0,0 +1,17 @@
+package com.osdu.client.delfi;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(url = "${osdu.delfi.portal.url}", name = "delfi.delivery.client")
+public interface DelfiDeliveryClient {
+
+    @GetMapping(value = "/storage/v2/records/{recordId}")
+    public void getRecord(@PathVariable("recordId") String recordId,
+                          @RequestHeader("Authorization") String authorizationToken,
+                          @RequestHeader("slb-data-partition-id") String partition,
+                          @RequestHeader("Slb-Account-Id") String accountId,
+                          @RequestHeader("AppKey") String applicationKey);
+}
