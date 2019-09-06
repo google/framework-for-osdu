d70d2edf34571294052fa6f339c725554f421e15
Rodion Shkrobot <rshkr@softserve.com>
2019-09-06 16:00:12 +0300

    OSDU-33: Mock service sample values

--- service/delivery/src/main/java/com/osdu/service/mock/MockSRNMappingService.java
+++ service/delivery/src/main/java/com/osdu/service/mock/MockSRNMappingService.java
@@ -1,10 +1,27 @@
 package com.osdu.service.mock;
 
 import com.osdu.service.SRNMappingService;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Temporary service to return Kinds while mapping is not yet implemented
+ */
+@Service
 public class MockSRNMappingService implements SRNMappingService {
+
+    private Map<String, String> srnToKindMap;
+
+    public MockSRNMappingService() {
+        srnToKindMap = new HashMap<>();
+        srnToKindMap.put("sampleSrn", "sampleKind");
+        srnToKindMap.put("yetAnotherSrn", "common:welldb:wellbore-dataset_descriptor-1.5");
+    }
+
     @Override
     public String mapSRNToKind(String srn) {
-        return null;
+        return srnToKindMap.get(srn);
     }
 }
