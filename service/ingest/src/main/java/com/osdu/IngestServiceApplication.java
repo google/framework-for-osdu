699b2d410bd5e96342349b8557158c6c8e1b45d0
Rodion Shkrobot <rshkr@softserve.com>
2019-09-12 17:36:27 +0300

    Ingest Service start.

---
+++ service/ingest/src/main/java/com/osdu/IngestServiceApplication.java
@@ -0,0 +1,16 @@
+package com.osdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = {"com.osdu"})
+public class IngestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestServiceApplication.class, args);
+    }
+
+}
