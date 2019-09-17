4e87ab2e83043330478e41154af6e0bbca17d513
Rodion Shkrobot <rshkr@softserve.com>
2019-09-17 14:55:37 +0300

    OSDU-15: SrnMappingService Mock impl, skeleton continued, lombok config

---
+++ service/ingest/src/main/java/com/osdu/service/delfi/DelfiIngestService.java
@@ -0,0 +1,13 @@
+package com.osdu.service.delfi;
+
+import com.osdu.service.IngestService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DelfiIngestService implements IngestService {
+
+  @Override
+  public void ingestManifest() {
+
+  }
+}
--- service/ingest/src/main/java/com/osdu/service/mock/MockSrnMappingService.java
+++ service/ingest/src/main/java/com/osdu/service/mock/MockSrnMappingService.java
@@ -2,19 +2,24 @@ package com.osdu.service.mock;
 
 import com.osdu.model.SchemaData;
 import com.osdu.service.SrnMappingService;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MockSrnMappingService implements SrnMappingService {
 
-  private Map<String, String> srnToKindMap;
-  private Map<String, List<SchemaData>> srnToSchemaDataMap;
+  Map<String, String> srnToKindMap;
+  Map<String, List<SchemaData>> srnToSchemaDataMap;
 
   public MockSrnMappingService() {
-    //TODO: Fill in mock data here
     srnToKindMap = new HashMap<>();
+    srnToKindMap.put("valid-srn-example", "valid-kind-result");
+    srnToKindMap.put("srn-with-no-schema", "kind-for-srn-with-no-schema");
     srnToSchemaDataMap = new HashMap<>();
+    srnToSchemaDataMap.put("valid-srn-example", Collections.singletonList(new SchemaData()));
   }
 
   @Override
