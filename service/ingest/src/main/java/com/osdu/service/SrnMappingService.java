5abb8be8fdb75cc4a54389c9ddfc74ee7aab456c
Rodion Shkrobot <rshkr@softserve.com>
2019-09-27 00:03:49 +0300

    OSDU-36: Mapper extracted into service

--- service/ingest/src/main/java/com/osdu/service/SrnMappingService.java
+++
@@ -1,8 +0,0 @@
-package com.osdu.service;
-
-import com.osdu.model.SchemaData;
-
-public interface SrnMappingService {
-  SchemaData getSchemaDataForSrn(String srn);
-  void saveSchemaData(SchemaData schemaData);
-}
