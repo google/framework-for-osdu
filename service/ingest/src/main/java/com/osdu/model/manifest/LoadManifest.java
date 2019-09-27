f7109f8158e2373e775f049e6f3345bafa3da97b
Pavlo Nesterenko <pnest@softserveinc.com>
2019-09-27 17:36:11 +0300

    OSDU-15: add additional properties field to load manifest

--- service/ingest/src/main/java/com/osdu/model/manifest/LoadManifest.java
+++ service/ingest/src/main/java/com/osdu/model/manifest/LoadManifest.java
@@ -1,12 +1,28 @@
 package com.osdu.model.manifest;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import lombok.Data;
 
 @Data
 public class LoadManifest {
 
+  @JsonProperty(value = "WorkProduct")
   Map<String, Object> workProduct;
-  Map<String, Object> files;
-  Map<String, Object> workProductComponents;
+
+  @JsonProperty(value = "Files")
+  List<Map<String, Object>> files;
+
+  @JsonProperty(value = "WorkProductComponents")
+  List<Map<String, Object>> workProductComponents;
+
+  Map<String, Object> additionalProperties = new LinkedHashMap<>();
+
+  @JsonAnySetter
+  void setDetail(String key, Object value) {
+    additionalProperties.put(key, value);
+  }
 }
