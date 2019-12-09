f8a3df3afb6d6ffb041311d01be275ddb3d5f3a1
Oleksii Davydov <odavydov@softserveinc.com>
2019-12-09 13:29:53 +0200

    Remove hardcoded values from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ Build and deploy a container to Cloud Run
   app       name of the microservice to build
   service   Cloud Run service name (default: same as app)
   region    Google Cloud region (default: us-central1)
-  
+
 EOF
   exit 1
 fi
@@ -37,14 +37,12 @@ REGION=$3
 
 
 if [[ -z $GOOGLE_CLOUD_PROJECT ]]; then
-  echo "Enter your GCP project ID (default - a2ba07aca58-energy-osdu):"
+  echo "Enter your GCP project ID:"
   read -r GOOGLE_CLOUD_PROJECT
 fi
 
-[[ -z $GOOGLE_CLOUD_PROJECT ]] && GOOGLE_CLOUD_PROJECT="a2ba07aca58-energy-osdu"
-gcloud config set project $GOOGLE_CLOUD_PROJECT
+gcloud config set project "$GOOGLE_CLOUD_PROJECT"
 
-[[ "$GOOGLE_CLOUD_PROJECT" = "a2ba07aca58-energy-osdu" ]] && CACHE_BUCKET="osdu-gcp-gitlab-cache"
 if [[ -z $CACHE_BUCKET ]]; then
   echo "Enter the GCS bucket for caching Cloud Build results"
   read -r CACHE_BUCKET
@@ -54,4 +52,4 @@ COMMIT_SHA=$(git rev-parse --short HEAD 2>/dev/null)
 [[ -z $COMMIT_SHA ]] && COMMIT_SHA=latest
 gcloud builds submit --config "${WORKDIR}"/cloudbuild.yaml --substitutions=_SERVICE_NAME="$APP",_SHORT_SHA="$COMMIT_SHA",_CACHE_BUCKET="$CACHE_BUCKET"
 
-gcloud beta run deploy "$SERVICE" --image gcr.io/${GOOGLE_CLOUD_PROJECT}/osdu-gcp-"${APP}":"${COMMIT_SHA}" --platform managed --region "$REGION"
+gcloud beta run deploy "$SERVICE" --image gcr.io/"${GOOGLE_CLOUD_PROJECT}"/osdu-gcp-"${APP}":"${COMMIT_SHA}" --platform managed --region "$REGION"
