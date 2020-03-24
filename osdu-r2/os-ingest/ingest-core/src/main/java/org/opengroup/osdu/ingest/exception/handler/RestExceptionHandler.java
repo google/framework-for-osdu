e109352b20bf06c3ec787bbd1164fe80f6dd30e6
Oleg Koskin <okosk@softserveinc.com>
2020-03-24 16:01:24 +0000

    Update OSDU R2

--- osdu-r2/os-ingest/ingest-core/src/main/java/org/opengroup/osdu/ingest/exception/handler/RestExceptionHandler.java
+++ osdu-r2/os-ingest/ingest-core/src/main/java/org/opengroup/osdu/ingest/exception/handler/RestExceptionHandler.java
@@ -32,6 +32,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -80,6 +81,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   protected ResponseEntity<Object> handleAppException(AppException e) {
     return this.getErrorResponse(e);
   }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+    ApiError apiError = ApiError.builder()
+        .status(status)
+        .message(ex.getLocalizedMessage())
+        .build();
+    return handleExceptionInternal(ex, apiError, headers, status, request);
+  }
+
 
   private ResponseEntity<Object> getErrorResponse(AppException e) {
 
